df606f4b165d170749a286e2b3a0e76ec5f287c9
Cameron Ross <elpenguino@gmail.com>
2012-02-16 16:18:02 -0400

    added more SPC700 opcodes small listdata fix

--- listdata.php
+++ listdata.php
@@ -8,8 +8,7 @@ function listdata() {
 			$realaddr = $platform->map_rom($addr);
 			if ($realaddr !== null)
 				$output[] = array('address' => isset($_GET['real_address']) ? $realaddr : $addr, 'type' => $data['type'], 'name' => !empty($data['name']) ? $data['name'] : '', 'description' => isset($data['description']) ? $data['description'] : '', 'size' => $data['size']);
-		} catch (Exception $e) {
-		}
+		} catch (Exception $e) { }
 	}
 	$dwoo = new Dwoo();
 	$dwoo->output('templates/listdata.tpl', array('game' => $gameid, 'title' => $game['title'], 'data' => $output));
