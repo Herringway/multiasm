728b647768835da92a590240818d6a602994c1b7
Cameron Ross <elpenguino@gmail.com>
2012-04-22 19:15:45 -0300

    Make hex viewer work again

--- mods/hexview.php
+++ mods/hexview.php
@@ -6,22 +6,22 @@ class hexview {
 		$this->main = Main::get();
 	}
 	public function execute() {
+		require 'libs/hexview.php';
 		if (!isset($this->main->addresses[$this->main->offset]['size']))
 			die('Data has no size defined!');
 		
-		rom::get()->seekTo(platform::get()->map_rom($this->main->offset));
-		$data = rom::get()->read_bytes($this->main->addresses[$this->main->offset]['size']);
 		$this->main->nextoffset = $this->main->decimal_to_function($this->main->offset+$this->main->addresses[$this->main->offset]['size']);
 		if (isset($this->main->addresses[$this->main->offset]['charset']))
-			$charset = Main::get()->game['texttables'][$this->addresses[$this->offset]['charset']]['replacements'];
+			$charset = Main::get()->game['texttables'][$this->addresses[$this->main->offset]['charset']]['replacements'];
 		else if (isset(Main::get()->game['defaulttext']))
 			$charset = Main::get()->game['texttables'][Main::get()->game['defaulttext']]['replacements'];
 		else
 			$charset = null;
 		if (isset($this->main->addresses[$this->main->offset]['description']))
 			$this->main->dataname = $this->main->addresses[$this->main->offset]['description'];
-		return array(array('charset' => $charset, 'width' => isset($this->main->addresses[$this->main->offset]['width']) ? $this->main->addresses[$this->main->offset]['width'] : 16),$data);
-		//return hexview($data, isset($this->main->addresses[$this->main->offset]['width']) ? $this->main->addresses[$this->main->offset]['width'] : 16, $this->main->offset, $charset);
+		rom::get()->seekTo(platform::get()->map_rom($this->main->offset));
+		$data = rom::get()->read($this->main->addresses[$this->main->offset]['size']);
+		return hexview($data, isset($this->main->addresses[$this->main->offset]['width']) ? $this->main->addresses[$this->main->offset]['width'] : 16, $this->main->offset, $charset);
 	}
 	public static function shouldhandle() {
 		if (isset(Main::get()->addresses[Main::get()->offset]['type']) && ((Main::get()->addresses[Main::get()->offset]['type'] === 'data') || (Main::get()->addresses[Main::get()->offset]['type'] === 'empty')) && !isset(Main::get()->addresses[Main::get()->offset]['entries']))
--- templates/hexview.tpl
+++ templates/hexview.tpl
@@ -1,6 +1,4 @@
 {extends "base.tpl"}
 {block "assembly"}
-	{for i 0 15}{string_format($i+$thisoffset%16, '%02X')} {/for}
-<hr>{string_format($thisoffset, '%06X')}: {loop $data.1}{string_format($, '%02X')} {if ($.loop.default.index % 16) == 15} 
-{string_format($_.thisoffset+$.loop.default.index+1, '%06X')}: {/if}{/loop}
+{$data}
 {/block}
