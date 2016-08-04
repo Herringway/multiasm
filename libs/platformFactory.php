a9da9fa7832ff9726023a3f837918f25c7a12053
Cameron Ross <elpenguino@gmail.com>
2016-08-04 16:44:21 -0300

    add platform caching logic

--- libs/platformFactory.php
+++ libs/platformFactory.php
@@ -1,9 +1,12 @@
 <?php
 class platformFactory {
+	static private $platforms = array();
 	static function _construct() { }
-	static function getPlatform($name) {
+	static function getPlatform($name, $id = 'default') {
 		require_once 'platforms/'.$name.'.php';
-		return new $name();
+		if (!isset(self::$platforms[$name.'/'.$id]))
+			self::$platforms[$name.'/'.$id] = new $name();
+		return self::$platforms[$name.'/'.$id];
 	}
 }
 
