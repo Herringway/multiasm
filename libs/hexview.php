a85b5f004b0e0022b1e98a9dbfef6bc3cfa171e4
Cameron Ross <elpenguino@gmail.com>
2016-08-04 16:59:21 -0300

    fix some string alignment issues in hexview

--- libs/hexview.php
+++ libs/hexview.php
@@ -1,22 +1,25 @@
 <?php
 function hexview($data,$wrap = 16, $baseaddress = 0, $charset = null) {
-	$output = '         ';
+	$output = '        ';
 	for ($i = 0; $i < $wrap; $i++)
 		$output .= sprintf('%02X ', ($baseaddress+$i)%$wrap);
 	$output .= sprintf('<hr>%06X: ', $baseaddress);
 	$ascii = '';
+	$stringlen = 0;
 	for ($i = 0; isset($data[$i]); $i++) {
 		$output .= sprintf('%02X ', ord($data[$i]));
+		$stringlen++;
 		$ascii .= replacechar($data[$i], $charset);
 		if ($i%$wrap == $wrap-1) {
 			$output .= sprintf("%s\r\n",$ascii);
-			if ($i+1 < strlen($data))
+			if ($i < strlen($data))
 				$output .= sprintf('%06X: ', $baseaddress+$i+1);
 			$ascii = '';
+			$stringlen = 0;
 		}
 	}
 	if ($ascii != null)
-		$output .= str_repeat(' ', ($wrap*3)-mb_strlen($ascii, 'UTF-8')*3).$ascii;
+		$output .= str_repeat(' ', ($wrap-$stringlen)*3).$ascii;
 	return $output;
 }
 
