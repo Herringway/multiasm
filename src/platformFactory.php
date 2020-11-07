8368fd2c0228a205ad2e31835d700f63cb874dd4
Cameron Ross <elpenguino@gmail.com>
2020-11-07 02:55:23 -0400

    make sure platform name is valid

--- src/platformFactory.php
+++ src/platformFactory.php
@@ -3,6 +3,7 @@ class platformFactory {
 	static private $platforms = array();
 	static function _construct() { }
 	static function getPlatform($name, $id = 'default') {
+		assert($name != "", "No name provided");
 		require_once 'src/platforms/'.$name.'.php';
 		if (!isset(self::$platforms[$name.'/'.$id]))
 			self::$platforms[$name.'/'.$id] = new $name();
